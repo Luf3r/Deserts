fb459e491d67e9f7a89c2c46e8cedff022fe95a6
lufer <zujev.daniel20319@gmail.com>
2021-04-09 17:36:55 +0300

    New class constructor for the deserts.

---
+++ Desery/src/main/java/com/example/deserts/DesertEntity.java
@@ -0,0 +1,7 @@
+package com.example.deserts;
+
+public class DesertEntity{
+    public Long id;
+    public String name;
+    public Float price;
+}
--- Desery/src/main/java/com/example/deserts/DessertsController.java
+++ Desery/src/main/java/com/example/deserts/DessertsController.java
@@ -11,11 +11,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DessertsController {
     
+//    @GetMapping("/api/desery")
+//    public List<String> getDesserts() {
+//        ArrayList<String> desserts=new ArrayList<>();
+//        Collections.addAll(desserts, new String[]{"donut","ics","froyo"});
+//        return desserts;
+//    }
+
     @GetMapping("/api/desery")
-    public List<String> getDesserts() {
-        ArrayList<String> desserts=new ArrayList<>();
-        Collections.addAll(desserts, new String[]{"donut","ics","froyo"});
+    public List<DesertEntity> getDesserts() {
+        ArrayList<DesertEntity> desserts=new ArrayList<>();
+        DesertEntity d1 = new DesertEntity();
+
+        d1.id = 0L;
+        d1.name = "Donut";
+        d1.price = 54f;
+
+        DesertEntity d2 = new DesertEntity();
+        d2.id = 4L;
+        d2.name = "Prokerka";
+        d2.price = 96f;
+
+        DesertEntity d3 = new DesertEntity();
+        d3.id = 5L;
+        d3.name = "Prok";
+        d3.price = 85.9f;
+
+        desserts.add(d1);
+        desserts.add(d2);
+        desserts.add(d3);
+
+        System.out.println("Returned the list");
         return desserts;
     }
-    
 }
