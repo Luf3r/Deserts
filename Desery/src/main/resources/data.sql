d0592f4e406c41c777d19b10e9b8c0a2a6951836
lufer <zujev.daniel20319@gmail.com>
2021-04-24 16:07:30 +0300

    new data

--- Desery/src/main/resources/data.sql
+++ Desery/src/main/resources/data.sql
@@ -1,4 +1,7 @@
 insert into deserts (id, name, price) values
 (null, 'Donut', 3),
 (null, 'Ice cream sandwich', 4),
-(null, 'Frozen youghurt', 4);
+(null, 'Frozen youghurt', 4),
+(null, '???', '???', 100),
+(null, '???2', '???2', 50),
+(null, '???3', '???3', 25);
